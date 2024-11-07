1bd2e623aa6dca7b18c11daa670fa25441543e57
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-11-07 10:06:14 -0400

    feat: retrieves server api docs with orval

--- src/components/empty-goals.tsx
+++ src/components/empty-goals.tsx
@@ -10,7 +10,8 @@ export function EmptyGoals() {
       <img src={logo} alt="in.orbit" />
       <img src={letsStart} alt="in.orbit" />
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
       </p>
 
       <DialogTrigger asChild>
